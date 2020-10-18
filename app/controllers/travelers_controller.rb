c5182407dbebd28f593529283014a06e069210b2
alexstephane <alexngundji@gmail.com>
2020-10-18 18:29:31 -0400

    create controller

--- app/controllers/travelers_controller.rb
+++ app/controllers/travelers_controller.rb
@@ -1,2 +1,11 @@
 class TravelersController < ApplicationController
+
+    def create 
+        
+     traveler =  Traveler.create(name:params[:name],current_location: Location.all.sample.name )
+     render json: traveler.to_json()
+        
+    end
+
+
 end
