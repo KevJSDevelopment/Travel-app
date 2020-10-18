8a7fd4487be0c84e397683e2a889e7ed2fa6feaa
KevJSDevelopment <Kshhn12@gmail.com>
2020-10-18 18:59:31 -0400

    create acc &  profile

--- app/controllers/travelers_controller.rb
+++ app/controllers/travelers_controller.rb
@@ -2,8 +2,8 @@ class TravelersController < ApplicationController
 
     def create 
         
-     traveler =  Traveler.create(name:params[:name],current_location: Location.all.sample.name )
-     render json: traveler.to_json()
+        traveler =  Traveler.create(name:params[:name],current_location: Location.all.sample.name )
+        render json: traveler.to_json()
         
     end
 
